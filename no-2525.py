b469268f7a02dd2988c1ccef951f3950a019ba1a
unknown <eodrmfdl1004@naver.com>
2022-03-05 13:21:29 +0900

    modify time calculate program

--- no-2525.py
+++ no-2525.py
@@ -4,11 +4,8 @@
 # 훈제오리구이를 시작하는 시각과 오븐구이를 하는 데 필요한 시간이 분단위로 주어졌을 때,
 # 오븐구이가 끝나는 시각을 계산하는 프로그램을 작성하시오.
 
-time = input().split()
+A, B = map(int, input().split())
 C = int(input())
-
-A = int(time[0])
-B = int(time[1])
 
 if B + C >= 60 :
     hour = (B + C) // 60
