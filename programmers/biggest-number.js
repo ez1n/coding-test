527a26ba3389af12979a7a19da4b77551658d4c4
unknown <eodrmfdl1004@naver.com>
2023-02-17 10:35:09 +0900

    Lv.2 가장 큰 수

---
+++ programmers/biggest-number.js
@@ -0,0 +1,22 @@
+/**
+ * Lv.2 가장 큰 수
+ *
+ * 0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
+ *
+ * 0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때,
+ * 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
+ *
+ * 제한 사항
+ *
+ * numbers의 길이는 1 이상 100,000 이하입니다.
+ * numbers의 원소는 0 이상 1,000 이하입니다.
+ * 정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
+ *
+ */
+
+function solution(numbers) {
+  const result = numbers.map(String).sort((a, b) => (b + a) - (a + b));
+
+  return result[0] === "0" ? "0" : result.join("");
+}
+
